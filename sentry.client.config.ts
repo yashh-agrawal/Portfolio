01c613923497428e1fd511080a4dc7c9a70171d0
yashagrawal13072000 <yashagrawal13072000@gmail.com>
2024-09-26 14:48:01 -0400

    sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -28,5 +28,6 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: true,
+  
 });
